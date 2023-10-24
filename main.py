136d6224fd9c70c1c88b42ff729bdde322fb08d7
Batra <kerilbatra2@gmail.com>
2023-10-24 20:23:21 +0500

    app

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.uix.image import Image
 from kivy.uix.popup import Popup
 
 
